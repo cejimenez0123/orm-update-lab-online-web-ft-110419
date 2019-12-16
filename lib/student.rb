a9c63fb0a34b2d8c7115910c3fb2a15d0fcdc76b
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-16 16:36:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,8 @@ class Student
   end
   def self.create_table
     sql = <<-SQL 
-     CREATE TABLE IF NOT EXISTS students
+     CREATE TABLE IF NOT EXISTS students (name,grade)
     SQL
+    DB
   end
 end
