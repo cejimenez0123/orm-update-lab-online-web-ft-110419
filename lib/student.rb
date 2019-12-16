1b2e2e982ebd5c9ca8d32f94c120f59050f4c17b
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-16 16:59:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
     grade = x[2]
     student = Student.new(id,name,grade)
   end
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = <<-SQL 
       SELECT * FROM students WHERE name = ? LIMIT 1
       SQL
